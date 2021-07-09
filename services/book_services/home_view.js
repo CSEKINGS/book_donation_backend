0e663202bc667c24a475b961805e7f1ffc3d9807
Sathiyanarayanan-M <sathiyastar2000@gmail.com>
2021-07-09 23:35:25 +0530

    Sort by Location in HomeView

--- services/book_services/home_view.js
+++ services/book_services/home_view.js
@@ -1,14 +1,37 @@
 const Book = require("../../models/book-model");
 
-
+/**
+ * HomeView 
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
 
 module.exports = async (req, res, next) => {
     nPerPage = 10;
     const pageNo = req.params.pageNo || 1;
     var book_ids = {}
-    const books = await Book.find().skip((nPerPage * (pageNo - 1))).limit(nPerPage);
+    let books;
+    try {
+        Book.collection.createIndex({ location: "2dsphere" });
+        books = await Book.find({
+            location: {
+                $near:
+                {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [74.297, 34.322]
+                    },
+                }
+            }
+        })
+        .skip((nPerPage * (pageNo - 1))).limit(nPerPage);
+    }
+    catch (err) {
+        console.log(err);
+    }
     books.forEach((a_book) => {
         book_ids[a_book._id] = a_book.name;
-    })
+    });
     return res.send(book_ids);
 }
