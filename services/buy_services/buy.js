1681219c2809dc2e43944076d46ae9595dd652d5
SivaPrakasam7 <prakasams22@gmail.com>
2021-07-09 00:31:06 -0400

    notification updated

--- services/buy_services/buy.js
+++ services/buy_services/buy.js
@@ -12,16 +12,16 @@ exports.book_buy = async (req, res, next) => {
         }
         else {
             if (book_r) {
-                User.findOne({ _id: userID },async (err, user_r) => {
+                User.findOne({ _id: userID }, async (err, user_r) => {
                     if (err) return next(err);
-                    mailer =await Mailer({ email: user_r.email, subject: `${book_r.name} requested by the user ${user_r.name}`, profile: "<h1>Testing...<h1>" });
-                    if(mailer.err){
+                    mailer = await Mailer({ email: user_r.email, subject: `${book_r.name} requested by the user ${user_r.name}`, profile: "<h1>Testing...<h1>" });
+                    if (mailer.err) {
                         err = mailer.err
                         err.code = 111
                         err.message = "Connection refused or inability to open an SMTP stream"
                         return next(err);
                     }
-                    else{
+                    else {
                         return res.status(200).send("Book requested successfully");
                     }
 
@@ -61,26 +61,27 @@ exports.book_owner = async (req, res, next) => {
 // Book request notification
 exports.book_notification = async (req, res, next) => {
     var user_book = {};
+    var notifications = {}
     const userID = req.decoded.id;
     await Book.find({ userID: userID }, "-location ", (err, books) => {
         if (err) {
             return next(err);
         } else {
             books.forEach((a_book) => {
-                for (var k of a_book.receiverID) {
+                for (var k in [...new Set(a_book.receiverID)]) {
                     user_book[k] = {
                         bookID: a_book._id,
                         title: a_book.name,
+                        receiver: a_book.receiverID[k],
                         requestTime: a_book.receivedTimestamp
                     };
                 }
             });
         }
     });
-    var i=1;
     for (var k in user_book) {
-        user_book[i]['user'] = await User.findOne({ _id: k }, "-_id -userLog -password", (user_r) => { return user_r });
-        i++;
+        console.log(user_book[k]);
+        user_book[k]['user'] = await User.findOne({ _id:  user_book[k].receiver}, "-_id -userLog -password", (user_r) => { return user_r });
     }
     return res.status(200).send(user_book);
 }
