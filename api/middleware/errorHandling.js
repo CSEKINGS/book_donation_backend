099b717d3cda87e830e5aca67734f6152a9a1b33
SivaPrakasam7 <prakasams22@gmail.com>
2021-09-19 19:02:06 +0530

    package-lock removed

--- api/middleware/errorHandling.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = async (err, req, res, next) => {
-    return res.status(err.code || 400).send({
-        err: true,
-        message: err.message || "Internal Server Error"
-    });
-}
--- configuration/dbConnection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = async () => {
-   await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,
-    useFindAndModify: true})
-    .then(() => console.log('Database Connected'))
-    .catch((err) => console.log(err));
-}
