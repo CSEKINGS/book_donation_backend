141d55db90b51f0e35c6991fec88ad781a78f72e
SivaPrakasam7 <prakasams22@gmail.com>
2021-12-02 15:11:52 +0530

    sendmail resolved

--- services/cart_services/buy.js
+++ services/cart_services/buy.js
@@ -3,44 +3,47 @@ const Book = require("../../models/book-model");
 const Mailer = require("../mail_services/sendMail");
 
 // Book donation status
-exports.book_buy = async(req, res, next) => {
-    const { bookId } = req.body;
+exports.book_buy = (req, res, next) => {
+    const { bookId, message } = req.body;
     const userID = req.decoded.id;
-    await Book.findOneAndUpdate({ _id: bookId, userID: { $ne: userID }, receiverID: { $ne: userID } }, { $push: { receiverID: userID } }, (err, book_r) => {
-        if (err) {
-            return next(err);
-        } else {
-            if (book_r) {
-                User.findOne({ _id: userID }, async(err, user_r) => {
-                    if (err) return next(err);
-                    mailer = await Mailer({ email: user_r.email, subject: `${book_r.name} requested by the user ${user_r.name}`, profile: "<h1>Testing...<h1>" });
-                    if (mailer.err) {
-                        err = mailer.err
-                        err.code = 111
-                        err.message = "Connection refused or inability to open an SMTP stream"
-                        return next(err);
-                    } else {
-                        return res.status(200).send("Book requested successfully");
-                    }
-
-                });
+    Book.findOneAndUpdate({ _id: bookId, userID: { $ne: userID }, receiverID: { $ne: userID } }, {
+            $push: { receiverID: { userID, message } }
+        },
+        (err, book_r) => {
+            if (err) {
+                return next(err);
             } else {
-                return next({ code: 404, message: "Book not found" });
+                if (book_r) {
+                    User.findOne({ _id: book_r.userID }, async(err, user_r) => {
+                        if (err) return next(err);
+                        mailer = await Mailer({ email: user_r.email, subject: `${book_r.name} book requested by ${user_r.name}`, profile: `<table><tr><td><img src="${user_r.photo}" style="height:50px;width:50px;border-radius:100%"></td></tr><tr><td><h1>${user_r.name}</h1></td></tr><tr><td><h2>Book name:${book_r.name}</h2></td></tr><tr><td><h3 style="margin:1">${message}</h3></td></tr></table>` });
+                        if (mailer.err) {
+                            err = mailer.err
+                            err.code = 111
+                            err.message = "Connection refused or inability to open an SMTP stream"
+                            return next(err);
+                        } else {
+                            return res.status(200).send("Book requested successfully");
+                        }
+
+                    });
+                } else {
+                    return next({ code: 404, message: "Book not found" });
+                }
             }
-        }
-    });
+        });
 }
 
 // View book owner detail after request
-exports.book_owner = async(req, res, next) => {
+exports.book_owner = (req, res, next) => {
     const { bookId } = req.body;
     const userID = req.decoded.id;
-    Book.findOne({ _id: bookId, receiverID: { $in: userID } }, "-receiverID -receivedTimestamp", async(err, book_r) => {
+    Book.findOne({ _id: bookId, receiverID: { $in: userID } }, "-receiverID -receivedTimestamp -wishedUsers", (err, book_r) => {
         if (err) {
             return next(err);
         } else {
             if (book_r) {
-                User.findOne({ _id: book_r.userID }, "-_id -password -userLog", async(err, user_r) => {
+                User.findOne({ _id: book_r.userID }, "-_id -password -userLog", (err, user_r) => {
                     if (err) {
                         return next(err);
                     } else {
--- services/mail_services/sendMail.js
+++ services/mail_services/sendMail.js
@@ -1,27 +1,28 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (data) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    auth: {
-      user: process.env.MAIL_ID,
-      pass: process.env.MAIL_PASS,
-    },
-  });
-
-  const mailOptions = {
-    to: data.email,
-    subject: data.subject,
-    html: data.profile,
-  };
-
-  return await transporter.sendMail(mailOptions).then((info) => {
-    return { "info": info }
-  })
-    .catch((err) => {
-      return { "err": err }
+const sendEmail = async(data) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        auth: {
+            user: process.env.MAIL_ID,
+            pass: process.env.MAIL_PASS,
+        },
     });
+
+    const mailOptions = {
+        from: "no-reply@book-donation.com",
+        to: data.email,
+        subject: data.subject,
+        html: data.profile,
+    };
+
+    return await transporter.sendMail(mailOptions).then((info) => {
+            return { "info": info }
+        })
+        .catch((err) => {
+            return { "err": err }
+        });
 
 };
 
